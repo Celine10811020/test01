5956ee13d79516a260f15121baf679eff43db740
Celine10811020 <shinecloudhill@gmail.com>
2024-06-17 15:41:12 +0800

    add from git push

--- hoho3_json.py
+++ hoho3_json.py
@@ -2,11 +2,11 @@ import os
 import json
 
 # Define the path to the hoho folder and JSON file
-hoho_path = 'hoho3'
+hoho3_path = 'hoho3'
 json_file_path = 'hoho3.json'
 
 # List all files in the hoho folder
-files = os.listdir(hoho_path)
+files = os.listdir(hoho3_path)
 
 # Filter for files that match the pattern 診所YYYYMMDD-MMDD.jpg
 filtered_files = [f for f in files if f.startswith('和和3-') and f.endswith('.jpg')]
@@ -16,7 +16,7 @@ json_data = []
 
 # Iterate over the filtered files and extract the necessary information
 for file_name in filtered_files:
-    path = os.path.join(hoho_path, file_name)
+    path = os.path.join(hoho3_path, file_name)
     # Extract the start and end dates
     start_date = file_name[4:12]
     end_date = file_name[4:8] + file_name[13:17]
